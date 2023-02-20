75f0af078e08846da154aed7591491c778a7b0bd
camilo ardila <miloardila98@gmail.com>
2023-02-19 22:24:09 -0500

    mostrando informacion en el modal

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ function iniciarApp() {
 
   const resultado = document.querySelector('#resultado');
 
+  //modal bootstrap
+
+  const modal = new bootstrap.Modal('#modal', {});
+
   obtenerCategorias();
 
   function obtenerCategorias() {
@@ -35,6 +39,14 @@ function iniciarApp() {
   }
 
   function mostrarRecetas( recetas = [] ) {
+
+    limpiarHTML(resultado);
+
+    const heading = document.createElement('H2');
+    heading.classList.add('text-center', 'text-black', 'my-5');
+    heading.textContent = recetas.length ? 'Resultados' : 'No hay resultados';
+    resultado.appendChild(heading);
+
     recetas.forEach(receta => {
       const { strMeal, idMeal, strMealThumb } = receta;
 
@@ -60,6 +72,10 @@ function iniciarApp() {
       recetaButton.classList.add('btn', 'btn-danger', 'w-100');
       recetaButton.textContent = 'Ver receta';
 
+      recetaButton.onclick = function (){
+        seleccionarReceta(idMeal);
+      }
+
       //texto y boton
       recetaCardBody.appendChild(recetaHeading);
       recetaCardBody.appendChild(recetaButton);
@@ -74,6 +90,57 @@ function iniciarApp() {
       resultado.appendChild(recetaContenedor);
 
     })
+  }
+
+  function seleccionarReceta(id) {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => mostrarRecetaModal(resultado.meals[0]))
+  }
+
+  function mostrarRecetaModal(receta) {
+    console.log(receta);
+    const { idMeal, strInstructions, strMeal, strMealThumb } = receta;
+
+    const modalTitle = document.querySelector('.modal .modal-title');
+    const modalBody = document.querySelector('.modal .modal-body');
+
+    modalTitle.textContent = strMeal;
+    modalBody.innerHTML = `
+      <img class="img-fluid" src="${strMealThumb}" alt="Receta ${strMeal}" />
+      <h3 class="my-3">Instrucciones</h3>
+      <p>${strInstructions}</p>
+      <h3 class="my-3">Ingredientes y cantidades</h3>
+    `;
+
+    //ul que alberga la lista de ingredientes
+    const listGroup = document.createElement('UL');
+    listGroup.classList.add('list-group');
+
+    //iterando sobre los ingredientes y las cantidades
+    for(let i = 1; i <= 20; i++) {
+      if(receta[`strIngredient${i}`]){
+        const ingrediente = receta[`strIngredient${i}`];
+        const cantidad = receta[`strMeasure${i}`];
+
+        const ingredienteLi = document.createElement('LI');
+        ingredienteLi.classList.add('list-group-item');
+        ingredienteLi.textContent = `${ingrediente} - ${cantidad}`;
+        console.log(ingredienteLi);
+        listGroup.appendChild(ingredienteLi);
+      }
+    }
+    modalBody.appendChild(listGroup);
+
+    //mostrar el modal
+    modal.show();
+  }
+
+  function limpiarHTML(selector) {
+    while(selector.firstChild){
+      selector.removeChild(selector.firstChild);
+    }
   }
 }
 
